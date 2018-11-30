d2b2d6a831481190d38d1fbc379bcaa53751b4bc
Nacho Harari <harari.ignacio@gmail.com>
2018-11-30 06:55:16 -0300

    comit

--- src/main/java/sts/saiyajin/cards/skills/FullMoon.java
+++ src/main/java/sts/saiyajin/cards/skills/FullMoon.java
@@ -24,7 +24,7 @@ public class FullMoon extends CustomCard {
 	
 	public FullMoon() {
 		super(CardNames.FULL_MOON, cardStrings.NAME, CardPaths.FULL_MOON, COST, cardStrings.DESCRIPTION, 
-		        AbstractCard.CardType.SKILL,
+		        AbstractCard.CardType.POWER,
 		        CardColors.SAIYAN_EXTRA_CARD_COLOR,
 		        AbstractCard.CardRarity.SPECIAL,
 		        AbstractCard.CardTarget.SELF);
