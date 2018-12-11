f7024771630cc9b60ad249c68941e78c937a1349
Nacho Harari <harari.ignacio@gmail.com>
2018-12-11 06:21:11 -0300

    comit

--- src/main/java/sts/saiyajin/ui/CardPaths.java
+++ src/main/java/sts/saiyajin/ui/CardPaths.java
@@ -9,22 +9,22 @@ public class CardPaths {
 	 */
 	public static final String SAIYAN_STRIKE = "img/cards/strike.png";
 	public static final String SAIYAN_DEFEND = "img/cards/block.png";
-	public static final String KI_BLAST = "img/cards/strike.png";
-	public static final String POWER_UP = "img/cards/block.png";
+	public static final String KI_BLAST = "img/cards/kiblast.png";
+	public static final String POWER_UP = "img/cards/powerup.png";
 
 	/**
 	 * COMMONS
 	 */
-	public static final String KAIO_KEN = "img/cards/block.png";
-	public static final String KAME_HAME_HA = "img/cards/strike.png";
-	public static final String GENKI_DAMA = "img/cards/strike.png";
-	public static final String METEOR_DASH = "img/cards/strike.png";
-	public static final String FLURRY = "img/cards/strike.png";
-	public static final String KI_EXPLOSION = "img/cards/block.png";
+	public static final String KAIO_KEN = "img/cards/kaioken.png";
+	public static final String KAME_HAME_HA = "img/cards/kamehameha.png";
+	public static final String GENKI_DAMA = "img/cards/genkidama.png";
+	public static final String METEOR_DASH = "img/cards/meteordash.png";
+	public static final String FLURRY = "img/cards/flurry.png";
+	public static final String KI_EXPLOSION = "img/cards/kiexplosion.png";
 	public static final String BURNING_SOUL = "img/cards/block.png";
 	public static final String CONCUSSIVE_BLOW = "img/cards/block.png";
-	public static final String QUICKENING = "img/cards/block.png";
-	public static final String TURTLE_SHELL = "img/cards/block.png";
+	public static final String QUICKENING = "img/cards/quickening.png";
+	public static final String TURTLE_SHELL = "img/cards/turtleshell.png";
 	public static final String SCOUTER = "img/cards/block.png";
 	public static final String CC_CAPSULE = "img/cards/block.png";
 	public static final String BACK_ATTACK = "img/cards/strike.png";
