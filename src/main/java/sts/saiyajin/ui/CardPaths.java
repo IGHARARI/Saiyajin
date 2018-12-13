9902cb705ca2836fbd9da2e76652cb6fb0c5f6a7
Nacho Harari <harari.ignacio@gmail.com>
2018-12-12 21:28:24 -0300

    comit

--- src/main/java/sts/saiyajin/ui/CardPaths.java
+++ src/main/java/sts/saiyajin/ui/CardPaths.java
@@ -21,35 +21,35 @@ public class CardPaths {
 	public static final String METEOR_DASH = "img/cards/meteordash.png";
 	public static final String FLURRY = "img/cards/flurry.png";
 	public static final String KI_EXPLOSION = "img/cards/kiexplosion.png";
-	public static final String BURNING_SOUL = "img/cards/block.png";
-	public static final String CONCUSSIVE_BLOW = "img/cards/block.png";
+	public static final String BURNING_SOUL = "img/cards/burningsoul.png";
+	public static final String CONCUSSIVE_BLOW = "img/cards/concussiveblow.png";
 	public static final String QUICKENING = "img/cards/quickening.png";
 	public static final String TURTLE_SHELL = "img/cards/turtleshell.png";
-	public static final String SCOUTER = "img/cards/block.png";
-	public static final String CC_CAPSULE = "img/cards/block.png";
-	public static final String BACK_ATTACK = "img/cards/strike.png";
-	public static final String EXTREME_SPEED = "img/cards/block.png";
-	public static final String BIG_BANG_ATTACK = "img/cards/strike.png";
-	public static final String SAIYAN_HUBRIS = "img/cards/block.png";
+	public static final String SCOUTER = "img/cards/scouter.png";
+	public static final String CC_CAPSULE = "img/cards/cccapsule.png";
+	public static final String BACK_ATTACK = "img/cards/backattack.png";
+	public static final String EXTREME_SPEED = "img/cards/extremespeed.png";
+	public static final String BIG_BANG_ATTACK = "img/cards/bigbang.png";
+	public static final String SAIYAN_HUBRIS = "img/cards/hubris.png";
 
 	/**
 	 * UNCOMMONS
 	 */
-	public static final String SOLAR_FLARE = "img/cards/block.png";
-	public static final String TELEPORT = "img/cards/block.png";
+	public static final String SOLAR_FLARE = "img/cards/taiyouken.png";
+	public static final String TELEPORT = "img/cards/teleport.png";
 	public static final String THIRST_FOR_FIGHT = "img/cards/block.png";
 	public static final String DRAINING_STRIKE = "img/cards/block.png";
-	public static final String ENDURE = "img/cards/block.png";
+	public static final String ENDURE = "img/cards/endure.png";
 	public static final String LAST_RESORT = "img/cards/block.png";
 	public static final String FLY = "img/cards/block.png";
 	
 	/**
 	 * RARES
 	 */
-	public static final String SUPER_SAIYAN_FORM = "img/cards/block.png";
+	public static final String SUPER_SAIYAN_FORM = "img/cards/supersaiyan.png";
 	public static final String TAUNT = "img/cards/block.png";
 	public static final String SENZU_BEAN = "img/cards/block.png";
-	public static final String MONKEY_TAIL = "img/cards/block.png";
+	public static final String MONKEY_TAIL = "img/cards/tail.png";
 	public static final String MEDICINE = "img/cards/block.png";
 	
 	/**
